d3aefc592722be7f87e94f8df6afd0bd14bd20d4
perfringis <kopko.mateusz@gmail.com>
2024-12-27 12:37:54 +0100

    add config for .env files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './controller/app.controller';
 import { AppService } from './service/app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
