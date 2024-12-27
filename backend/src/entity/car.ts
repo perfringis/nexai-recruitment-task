274542f790b8ef01093626ae93b958521c47b9f4
perfringis <kopko.mateusz@gmail.com>
2024-12-27 19:27:48 +0100

    add implementation of car

--- backend/src/entity/car.ts
+++ backend/src/entity/car.ts
@@ -1,7 +1,30 @@
 import { LicensePlate } from './license.plate';
+import { VehicleIdentificationNumber } from './vehicle.identification.number';
 
 export class Car {
   private readonly brand: string;
   private readonly licensePlate: LicensePlate;
-  private readonly vehicleIdentificationNumber: VehicleIdentificationNumber;
+  private readonly vin: VehicleIdentificationNumber;
+
+  constructor(
+    brand: string,
+    licensePlate: LicensePlate,
+    vin: VehicleIdentificationNumber,
+  ) {
+    this.brand = brand;
+    this.licensePlate = licensePlate;
+    this.vin = vin;
+  }
+
+  public getBrand(): string {
+    return this.brand;
+  }
+
+  public getLicensePlate(): LicensePlate {
+    return this.licensePlate;
+  }
+
+  public getVIN(): VehicleIdentificationNumber {
+    return this.vin;
+  }
 }
