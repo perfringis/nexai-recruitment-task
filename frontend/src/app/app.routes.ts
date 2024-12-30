e99e475faaba661295da260a5df73e33e2e8ee49
perfringis <kopko.mateusz@gmail.com>
2024-12-30 17:20:59 +0100

    add pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'customer',
+    loadComponent: () =>
+      import('./pages/customer/customer.component').then((m) => m.CustomerComponent),
+  },
+  {
+    path: 'car',
+    loadComponent: () =>
+      import('./pages/car/car.component').then((m) => m.CarComponent),
+  },
+  {
+    path: 'rental',
+    loadComponent: () =>
+      import('./pages/rental/rental.component').then((m) => m.RentalComponent),
+  },
+];
